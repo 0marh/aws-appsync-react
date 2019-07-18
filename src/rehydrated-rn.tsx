2cfb4e41a30d111adc3038fd5fb327978fb1921b
Omar Hassan <omarh.abdalah@gmail.com>
2019-07-18 13:15:41 +0400

    Update rehydrated-rn.tsx

--- src/rehydrated-rn.tsx
+++ src/rehydrated-rn.tsx
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import * as React from "react";
-import { View, Text, StyleSheet, NetInfo, ViewPropTypes } from "react-native";
+import { View, Text, StyleSheet, ViewPropTypes } from "react-native";
+import * as NetInfo from "@react-native-community/netinfo" // NetInfo
+
 import * as PropTypes from 'prop-types';
 
 import AWSAppSyncClient from 'aws-appsync';
@@ -55,7 +57,7 @@ export default class Rehydrated extends React.Component<RehydratedProps, Rehydra
         };
     }
 
-    async componentWillMount() {
+    async componentDidMount() { // componentWillMount was deprecated
         await this.context.client.hydrated();
         await NetInfo.isConnected.fetch();
 
